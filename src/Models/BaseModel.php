5c2e9e60a2d68d3fc3a71ac8542c25c69d2225ee
Calebemesquita <calebemesquita.13@gmail.com>
2026-06-02 15:50:00 -0300

    feat: base do MVC

---
+++ src/Models/BaseModel.php
@@ -0,0 +1,31 @@
+<?php
+namespace Models;
+
+use PDO;
+use Core\Database;
+
+abstract class BaseModel {
+    protected PDO $db;
+    protected string $table; // Será definida pelas classes filhas
+
+    public function __construct() {
+        $this->db = Database::getConnection();
+    }
+
+    public function findAll() : array {
+        $stmt = $this->db->prepare("SELECT * FROM {$this->table}");
+        $stmt->execute();
+        return $stmt->fetchAll();
+    }
+
+    public function findById(int $id) : array {
+        $stmt = $this->db->prepare("SELECT * FROM {$this->table} WHERE id = :id");
+        $stmt->execute(['id' => $id]);
+        return $stmt->fetch() ?: [];
+    }
+
+    public function delete(int $id): bool {
+        $stmt = $this->db->prepare("DELETE FROM {$this->table} WHERE id = :id");
+        return $stmt->execute(['id' => $id]);
+    }
+}
